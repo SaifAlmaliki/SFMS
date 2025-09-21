ad0276c1e51bf3527e923111b5abcf343b37a90e
Ahmed Al hajy <8lbiuae@gmail.com>
2025-09-21 12:41:58 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={cn('font-body antialiased', 'dark')}>
+      <body className={cn('font-body antialiased', 'dark')} suppressHydrationWarning>
         <SidebarProvider>
           <AppLayout>{children}</AppLayout>
         </SidebarProvider>
